73bafd4253eec54a32ddee53ccfac017fee4c2da
Park YongJune <redplus@redplus.net>
2025-08-03 17:08:14 +0900

    Add IDepartmentRepository interface with async CRUD and paging methods

---
+++ src/Azunt.DepartmentManagement/Azunt.DepartmentManagement/02_Contracts/IDepartmentRepository.cs
@@ -0,0 +1,13 @@
+﻿using Azunt.Models.Common;
+
+namespace Azunt.DepartmentManagement;
+
+public interface IDepartmentRepository
+{
+    Task<Department> AddAsync(Department model, string? connectionString = null);
+    Task<List<Department>> GetAllAsync(string? connectionString = null);
+    Task<Department> GetByIdAsync(long id, string? connectionString = null);
+    Task<bool> UpdateAsync(Department model, string? connectionString = null);
+    Task<bool> DeleteAsync(long id, string? connectionString = null);
+    Task<ArticleSet<Department, int>> GetArticlesAsync<TParentIdentifier>(int pageIndex, int pageSize, string searchField, string searchQuery, string sortOrder, TParentIdentifier parentIdentifier, string? connectionString = null);
+}
