1caeca849f58f2938fa0f500615aebc6bd6cba2d
Park YongJune <redplus@redplus.net>
2025-08-07 01:59:15 +0900

    Update Azunt NuGet package and Remove DbProvider class file

--- src/Azunt.DepartmentManagement/Azunt.DepartmentManagement/03_Repositories/EfCore/DepartmentDbContextFactory.cs
+++ src/Azunt.DepartmentManagement/Azunt.DepartmentManagement/03_Repositories/EfCore/DepartmentDbContextFactory.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Azunt.Models.Enums;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
 namespace Azunt.DepartmentManagement;
